d8dcd61a005de9d747328c26e5ed23bca38b0354
ChrishonCrum <chrishonkcrum@yahoo.com>
2022-04-06 21:14:50 -0500

    commit tree f32a56e8e20474c437080b0b2cfb4f5bc497ac3a

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+const newHeader = document.createElement('h1')
+document.querySelector('#main').remove()
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
